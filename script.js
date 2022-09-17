860c5530d2db7fb0e0231d89dfde6d65304f1217
gleicebsouza <gleice_bsouza@hotmail.com>
2022-09-17 14:37:36 -0300

    modified-script

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
 // ERROR LINHA 68 :if (showAlertError) {
-/* 
-Propriedades que são Objetos:
-[] DOM - Document Object Model
-[#] OBJETOS
-  - .classList 
-  - querySelector
-[#] PROGRAMAÇÃO IMPERATIVA : maneiras de pensar, dar ordens do passo a passo do que será realizado
-[#]Event-driven
-  - eventos
-[#] FUNÇÕES
-  - Anonima
-  - Arrow function 
-[#] FUNÇÕES CALLBACKS 
-  - chamar a função de volta
-[#] REFATORAÇÃO
-  - Mudar o código sem alterar o seu comportamento
-  - deixar o código performatico
 
-*/
-/* 3 MANEIRAS DE CRIAR E ATRIBUIR FUNÇÃO A UM EVENTO:
-
-1. FUNÇÃO ANONIMA
-form.onsubmit = function(){}
-
-2.ARROW FUNCTION
-form.onsubmit = () => {}
-
-3.FUNÇÃO NAMED
-form.onsubmit = handleSubmit
-function handleSubmit(){}
-*/
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
 import { calculateIMC, notANumber } from './utils.js'
